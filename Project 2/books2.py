f95e4e0a2acfa0c22f7ab5ea4e60469735ae3c15
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-01-19 23:07:49 +0530

    code updated

--- Project 2/books2.py
+++ Project 2/books2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Path, Query, HTTPException, Body
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
 from starlette import status
 
@@ -24,7 +24,7 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = Field(description='ID is not needed on create', default=None)
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
@@ -38,19 +38,21 @@ class BookRequest(BaseModel):
                 "author": "codingwithroby",
                 "description": "A new description of a book",
                 "rating": 5,
-                'published_date': 2029
+                "published_date": 2029,
             }
         }
     }
 
+
 BOOKS = [
-    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2030),
-    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5, 2030),
-    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5, 2029),
-    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2028),
-    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2027),
-    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2026)
+    Book(1, "Computer Science Pro", "codingwithroby", "A very nice book!", 5, 2030),
+    Book(2, "Be Fast with FastAPI", "codingwithroby", "A great book!", 5, 2030),
+    Book(3, "Master Endpoints", "codingwithroby", "A awesome book!", 5, 2029),
+    Book(4, "HP1", "Author 1", "Book Description", 2, 2028),
+    Book(5, "HP2", "Author 2", "Book Description", 3, 2027),
+    Book(6, "HP3", "Author 3", "Book Description", 1, 2026),
 ]
+
 
 @app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
@@ -62,7 +64,7 @@ async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
-    raise HTTPException(status_code=404, detail='Item not found')
+    raise HTTPException(status_code=404, detail="Item not found")
 
 
 @app.get("/books/", status_code=status.HTTP_200_OK)
@@ -102,7 +104,7 @@ async def update_book(book: BookRequest):
             BOOKS[i] = book
             book_changed = True
     if not book_changed:
-        raise HTTPException(status_code=404, detail='Item not found')
+        raise HTTPException(status_code=404, detail="Item not found")
 
 
 @app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -114,4 +116,4 @@ async def delete_book(book_id: int = Path(gt=0)):
             book_changed = True
             break
     if not book_changed:
-        raise HTTPException(status_code=404, detail='Item not found')
+        raise HTTPException(status_code=404, detail="Item not found")
